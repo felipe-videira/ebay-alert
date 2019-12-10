8b96d5576fbbff3ebe9627a7b94f8a1eec9b826b
Felipe Videira <videirafelipe020@gmail.com>
2019-12-09 22:38:46 -0300

    in progress

---
+++ client/src/screens/AlertForm/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import React, { Component } from 'react';
+
+class AlertForm extends Component {
+
+  render () {
+      return (
+        <div className="App">
+        </div>
+      );
+  }
+}
+
+export default AlertForm;
---
+++ client/src/screens/AlertList/index.js
@@ -0,0 +1,95 @@
+import './style.css';
+import axios from 'axios';
+import { Table, Button, message } from 'antd';
+import React, { Component } from 'react';
+
+class AlertList extends Component {
+
+  state = {
+    loading: false,
+    data: [],
+    columns: [
+      { title: 'Email', dataIndex: 'email' },
+      { title: 'Search Phrase', dataIndex: 'searchPhrase' },
+      { title: 'Frequency', dataIndex: 'frequency' },
+      { 
+        render: (text, item) => <Button 
+          type="primary" 
+          shape="circle" 
+          icon="edit" 
+          onClick={() => this.editItem(item)} 
+        /> 
+      },
+      { 
+        render: (text, item) => <Button 
+          type="primary" 
+          shape="circle" 
+          icon="delete" 
+          onClick={() => this.deleteItem(item)} 
+        /> 
+      },
+    ]
+  }
+
+  componentDidMount () {
+    this.getItems()
+  }
+
+  async getItems () {
+    try {
+      this.setState({ loading: true });
+      
+      const { data } = await axios.get(process.env.API_HOST);
+      
+      this.setState({ data });
+
+    } catch (error) {
+      message.error("An error occurred please try again later");
+
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  async deleteItem (item) {
+    try {
+      this.setState({ loading: true });
+      
+      const { data } = await axios.delete(`${process.env.API_HOST}/${item.id}`);
+      
+      this.setState({ data });
+
+    } catch (error) {
+      message.error("An error occurred please try again later");
+
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  createItem () {
+    this.props.history.push('/alert');
+  }
+  
+  editItem (item) {
+    this.props.history.push('/alert', { id: item.id });
+  }
+
+  render () {
+    const { 
+      columns, 
+      data,
+      loading,
+    } = this.state; 
+
+    return (
+      <Table 
+        columns={columns} 
+        dataSource={data}
+        loading={loading} 
+      />  
+    );
+  }
+}
+
+export default AlertList;
